73b6bcf4756b878fe7b876e0312daa4fce768830
Nikachu <75615396+nikachu2012@users.noreply.github.com>
2022-08-03 02:21:44 +0900

    [add] rotateの追加

--- 02_sprite/rotate.js
+++ 02_sprite/rotate.js
@@ -1,4 +1,37 @@
 /**
+ * 指定の角度に変更します。右回転は+,左回転では-をつけてください。
+ * @param {string} id 
+ * @param {number} deg 
+ */
+emojisp.rotate = (id, deg) => {
+    try {
+        const rotateElement = document.getElementById(`emojiSprite_${id}`)
+
+        switch (emojisp.accessSpriteData(id).rotateType) {
+            case "lr":
+                if (deg == 0 || deg == 180) {
+                    rotateElement.style.transform = `rotate(${deg}deg)`
+                    spriteOption[id].deg = deg
+                }
+                break;
+            case "none":
+                break;
+            case "free":
+                rotateElement.style.transform = `rotate(${deg}deg)`
+                spriteOption[id].deg = deg
+                break;
+
+            default:
+                break;
+        }
+
+
+    } catch (error) {
+        alert('EmojiCode Sprite Controller Error detect!\nPlease see DevTools.')
+        console.log(error)
+    }
+}
+/**
  * 端に着いたら跳ね返る条件を計算します
  * @param {String} id 
  */
