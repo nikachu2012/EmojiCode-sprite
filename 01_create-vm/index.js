cd51babb50c0714713a012f0ab8374e15aa30daa
Nikachu <75615396+nikachu2012@users.noreply.github.com>
2022-08-03 02:19:45 +0900

    [add] createの追加

---
+++ 01_create-vm/index.js
@@ -0,0 +1,22 @@
+const emojisp = {};
+/**
+ * スプライト用のフィールドを書き込みます。
+ * @param {string} id idをつけたdivタグを指定してください。
+ * @param {JSON} option オプションのJSONを指定してください。
+ */
+emojisp.create = (id, option) => {
+    try {
+        writeElement = document.getElementById(id);
+
+        WIDTH = writeElement.clientWidth;
+        HEIGHT = writeElement.clientHeight;
+        
+        writeElement.style.position = "relative";
+        writeElement.style.backgroundColor = option.firstBackground;
+        writeElement.style.overflow = `hidden`;
+    } catch (error) {
+        alert('EmojiCode Sprite Controller Error detect!\nPlease see DevTools.')
+        console.log(error)
+    }
+} 
+
