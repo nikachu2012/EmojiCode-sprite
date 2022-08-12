46cabe376838e8a7199c50b6cce6d64bee9a02cb
Nikachu <75615396+nikachu2012@users.noreply.github.com>
2022-08-12 23:52:34 +0900

    [update] スプライトが重複しているときのエラーを追加

--- 02_sprite/create.js
+++ 02_sprite/create.js
@@ -6,25 +6,31 @@ let spriteOption = {};
 emojisp.createSprite = (data) => {
     try {
 
-        spriteOption[data.id] = data;
-
-        // 新しいHTML要素を作成
-        let new_element = document.createElement('img');
-
-        new_element.src = data.url;
-        new_element.id = `emojiSprite_${data.id}`
-        new_element.style.position = "absolute";
-        new_element.style.pointerEvents = "none";
-        if(!data.x > WIDTH || !data.y > HEIGHT){
-            new_element.style.top = `${data.y}px`;
-            new_element.style.left = `${data.x}px`;
+        if (Object.keys(spriteOption).includes(data.id)) {
+            alert('すでにスプライトが作成されています。')
+            console.log('Emojicode-Sprite Error: すでにスプライトが作成されています。')
         }
-        
-        new_element.style.width = `${data.width}px`
-        new_element.style.height = `${data.height}px`
+        else {
+            spriteOption[data.id] = data;
 
-        // 指定した要素の中の末尾に挿入
-        writeElement.appendChild(new_element);
+            // 新しいHTML要素を作成
+            let new_element = document.createElement('img');
+
+            new_element.src = data.url;
+            new_element.id = `emojiSprite_${data.id}`
+            new_element.style.position = "absolute";
+            new_element.style.pointerEvents = "none";
+            if (!data.x > WIDTH || !data.y > HEIGHT) {
+                new_element.style.top = `${data.y}px`;
+                new_element.style.left = `${data.x}px`;
+            }
+
+            new_element.style.width = `${data.width}px`
+            new_element.style.height = `${data.height}px`
+
+            // 指定した要素の中の末尾に挿入
+            writeElement.appendChild(new_element);
+        }
 
     } catch (error) {
         alert('EmojiCode Sprite Controller Error detect!\nPlease see DevTools.')
