34994706dc48b21d8574cb935bc2807167fb2859
Nikachu <75615396+nikachu2012@users.noreply.github.com>
2022-08-15 18:48:23 +0900

    [add] editで縦横比が計算されるように変更

--- 02_sprite/edit.js
+++ 02_sprite/edit.js
@@ -5,10 +5,26 @@
  */
 emojisp.editSprite = (id, option) => {
     try {
-        spriteOption[id].width = option.width;
-        spriteOption[id].height = option.height;
-        document.getElementById(`emojiSprite_${id}`).style.width = `${option.width}px`;
-        document.getElementById(`emojiSprite_${id}`).style.height = `${option.height}px`;
+
+
+
+        if (option.width !== 0) {
+            spriteOption[id].width = option.width;
+            document.getElementById(`emojiSprite_${id}`).style.width = `${option.width}px`;
+        }
+        else if (option.width == 0) {
+            spriteOption[id].width = 0;
+            document.getElementById(`emojiSprite_${id}`).style.width = null;
+        }
+
+        if (option.height !== 0) {
+            spriteOption[id].height = option.height;
+            document.getElementById(`emojiSprite_${id}`).style.height = `${option.height}px`;
+        }
+        else if (option.height == 0) {
+            spriteOption[id].height = 0;
+            document.getElementById(`emojiSprite_${id}`).style.height = null;
+        }
 
         spriteOption[id].url = option.url;
         document.getElementById(`emojiSprite_${id}`).src = option.url
