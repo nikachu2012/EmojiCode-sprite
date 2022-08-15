9ef166eea5de0f755e51f8548776fc7359e86f28
Nikachu <75615396+nikachu2012@users.noreply.github.com>
2022-08-15 15:05:28 +0900

    [add] 0の時自動計算されるように変更

--- 02_sprite/create.js
+++ 02_sprite/create.js
@@ -20,13 +20,35 @@ emojisp.createSprite = (data) => {
             new_element.id = `emojiSprite_${data.id}`
             new_element.style.position = "absolute";
             new_element.style.pointerEvents = "none";
-            if (!data.x > WIDTH || !data.y > HEIGHT) {
+            if (data.y <= HEIGHT) {
                 new_element.style.top = `${data.y}px`;
-                new_element.style.left = `${data.x}px`;
+            }
+            else {
+                new_element.style.top = `${HEIGHT - 10}px`;
             }
 
-            new_element.style.width = `${data.width}px`
-            new_element.style.height = `${data.height}px`
+            if (data.x <= WIDTH) {
+                new_element.style.left = `${data.x}px`;
+            }
+            else {
+                new_element.style.left = `${WIDTH - 10}px`;
+            }
+
+            if(data.width !== 0){
+                new_element.style.width = `${data.width}px`
+            }
+            else if(data.width == 0){
+                new_element.style.width = undefined
+            }
+
+            if(data.height !== 0){
+                new_element.style.height = `${data.height}px`
+            }
+            else if(data.height == 0){
+                new_element.style.height = undefined
+            }
+            
+            
 
             // 指定した要素の中の末尾に挿入
             writeElement.appendChild(new_element);
