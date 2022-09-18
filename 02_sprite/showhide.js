d1b483812cee2a63eac07be5f5651c13b78e686a
Nikachu <75615396+nikachu2012@users.noreply.github.com>
2022-09-19 03:27:58 +0900

    [add] emojisp.hideの追加

--- 02_sprite/showhide.js
+++ 02_sprite/showhide.js
@@ -5,3 +5,9 @@ emojisp.show = (id) => {
     dom.style.visibility = 'visible';
 }
 
+emojisp.hide = (id) => {
+    const dom = document.getElementById(`emojiSprite_${id}`);
+
+    spriteOption[id].visibility = false;
+    dom.style.visibility = 'hidden'
+}
